11bd427797e6ff93f468319859d90ab830a48af5
Gabriel Santos <gabrildado08@gamil.com>
2022-12-02 20:44:21 -0300

    update

--- js.js
+++ js.js
@@ -7,17 +7,54 @@ let box5 = document.getElementById("5")
 let box6 = document.getElementById("6")
 let box7 = document.getElementById("7")
 let box8 = document.getElementById("8")
+let w = document.getElementById("w")
+let b = document.getElementsByClassName("box")
 
-let game =["","","","","","","",""];
+let game =["","","","","","","","",""];
 let simbo = ["X","O"];
 let vez = 1;
 
-
+function win(){
+    if (game[0] ==="X"&&game[1]==="X"&&game[2]==="X" || game[0] ==="O"&&game[1]==="O"&&game[2]==="O" ){
+        
+        w.innerHTML = "Win!!!!"
+        return true
+        
+        
+        
+    }else if(game[3] ==="X"&&game[4]==="X"&&game[5]==="X" || game[3] ==="O"&&game[4]==="O"&&game[5]==="O"){
+        
+        w.innerHTML = "Win!!!!"
+        return true
+        
+    }else if(game[6] ==="X"&&game[7]==="X"&&game[8]==="X" || game[6] ==="O"&&game[7]==="O"&&game[8]==="O"){
+        
+        w.innerHTML = "Win!!!!"
+        return true
+    }else if(game[0] ==="X"&&game[3]==="X"&&game[6]==="X" || game[0] ==="O"&&game[3]==="O"&&game[6]==="O"){
+        w.innerHTML = "Win!!!!"
+        return true
+    }else if(game[1] ==="X"&&game[4]==="X"&&game[7]==="X" || game[1] ==="O"&&game[4]==="O"&&game[7]==="O"){
+        w.innerHTML = "Win!!!!"
+        return true
+    }else if(game[2] ==="X"&&game[5]==="X"&&game[8]==="X" || game[2] ==="O"&&game[5]==="O"&&game[8]==="O"){
+        w.innerHTML = "Win!!!!"
+        return true
+    }else if(game[0] ==="X"&&game[4]==="X"&&game[8]==="X" || game[0] ==="O"&&game[4]==="O"&&game[8]==="O"){
+        w.innerHTML = "Win!!!!"
+        return true
+    }else if(game[2] ==="X"&&game[4]==="X"&&game[6]==="X" || game[2] ==="O"&&game[4]==="O"&&game[6]==="O"){
+        w.innerHTML = "Win!!!!"
+        return true
+    }else{
+        return false
+    }
+}
 
 
 box0.addEventListener("click", function(){
     
-    if(game[0]===""){
+    if(game[0]==="" && win()===false){
     if (vez=== 1){
         game[0] = simbo[0]
         box0.innerHTML = game[0]
@@ -29,10 +66,11 @@ box0.addEventListener("click", function(){
         vez = 1
     }
     }
+    win()
 })
 
 box1.addEventListener("click", function(){
-    if(game[1]===""){
+    if(game[1]==="" && win()===false){
     if (vez=== 1){
         game[1] = simbo[0]
         box1.innerHTML = game[1]
@@ -43,10 +81,11 @@ box1.addEventListener("click", function(){
         vez =1
     }
 }
+win()
 })
 
 box2.addEventListener("click", function(){
-    if(game[2]===""){
+    if(game[2]==="" && win()===false){
     if (vez=== 1){
         game[2] = simbo[0]
         box2.innerHTML = game[2]
@@ -57,10 +96,11 @@ box2.addEventListener("click", function(){
         vez = 1
     }
     }
+    win()
 })
 
 box3.addEventListener("click", function(){
-    if(game[3]==="")
+    if(game[3]==="" && win()===false)
     if (vez=== 1){
         game[3] = simbo[0]
         box3.innerHTML = game[3]
@@ -71,10 +111,11 @@ box3.addEventListener("click", function(){
 
         vez = 1
     }
+    win()
 })
 
 box4.addEventListener("click", function(){
-    if(game[4]==="")
+    if(game[4]==="" && win()===false)
     if (vez=== 1){
         game[4] = simbo[0]
         box4.innerHTML = game[4]
@@ -84,10 +125,11 @@ box4.addEventListener("click", function(){
         box4.innerHTML = game[4]
         vez = 1
     }
+    win()
 })
 
 box5.addEventListener("click", function(){
-    if(game[5]==="")
+    if(game[5]==="" && win()===false)
     if (vez=== 1){
         game[5] = simbo[0]
         box5.innerHTML = game[5]
@@ -97,10 +139,11 @@ box5.addEventListener("click", function(){
         box5.innerHTML = game[5]
         vez = 1
     }
+    win()
 })
 
 box6.addEventListener("click", function(){
-    if(game[6]==="")
+    if(game[6]==="" && win()===false)
     if (vez===1){
         game[6] = simbo[0]
         box6.innerHTML = game[6]
@@ -110,10 +153,11 @@ box6.addEventListener("click", function(){
         box6.innerHTML = game[6]
         vez = 1
     }
+    win()
 })
 
 box7.addEventListener("click", function(){
-    if(game[7]==="")
+    if(game[7]==="" && win()===false)
     if (vez===1){
         game[7] = simbo[0]
         box7.innerHTML = game[7]
@@ -124,10 +168,11 @@ box7.addEventListener("click", function(){
 
         vez = 1
     }
+    win()
 })
 
 box8.addEventListener("click", function(){
-    if (game[8]!="X"&&game[8]!="O")
+    if (game[8]!="X"&&game[8]!="O" && win()===false)
     if (vez=== 1){
         game[8] = simbo[0]
         box8.innerHTML = game[8]
@@ -137,4 +182,6 @@ box8.addEventListener("click", function(){
         box8.innerHTML = game[8]
         vez = 1
     }
+    win()
 })
+
